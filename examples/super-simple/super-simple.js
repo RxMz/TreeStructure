9bcaf60c07f200f1c0064e650b4227f50b641b16
Rishabh <rishabhminocha99900@gmail.com>
2018-07-03 15:37:46 -0400

    Updates

--- examples/super-simple/super-simple.js
+++ examples/super-simple/super-simple.js
@@ -2,9 +2,49 @@
 
 // // // // // // // // // // // // // // // // // // // // // // // // 
 
+
+var map = new HashMap();
+
+
 var config = {
 	container: "#OrganiseChart-simple"
 };
+
+/*
+
+data = '[{"name" : "Ashwin", "age" : "20"},{"name" : "Abhinandan", "age" : "20"}]';
+
+var mydata = JSON.parse(data);
+alert(mydata[0].name);
+alert(mydata[0].age);
+alert(mydata[1].name);
+alert(mydata[1].age);
+*/
+
+/*
+var heirarchy = "";
+var nodes = [];
+var info ="";
+
+
+// To create the nodes of the tree
+for (var i = 0; i <= info.length; i++) {
+	var temp_node = {
+		text: { name: info[i].name}
+	};
+	nodes.push(temp_node);
+}
+ 
+
+// To connect the nodes
+for (var i = 0; i <= info.length; i++) {
+	temp_node[] = {
+		text: { name: info[i].name}
+	};
+	nodes.push(temp_node);
+}
+
+*/
 
 var parent_node = {
 	text: { name: "Parent node" }
@@ -22,13 +62,13 @@ var second_child = {
 
 var third = {
 	parent: second_child,
-	text: { name: "Second child" }
+	text: { name: "Thirs child" }
 };
 
 
 var four = {
 	parent: second_child,
-	text: { name: "Second child" }
+	text: { name: "Fourth child" }
 };
 
 
@@ -43,12 +83,24 @@ var six = {
 };
 
 var seven = {
-	parent: six,
-	text: { name: "Second child" }
+	parent: third,
+	text: { name: "Second childdd" }
 };
 
+var eight = {
+	parent: third,
+	text: {name: "Eigth Child"}
+};
 
-var simple_chart_config = [
-	config, parent_node,
-		first_child, second_child, third, four, five, six, seven
+var test_node = {
+	text: {name: "Test node"}
+};
+
+test_node = {
+	parent: parent_node,
+	text: {name: "Test node updatd"}
+};
+
+var simple_chart_config = [config, first_child, parent_node , second_child,
+ third, four, five, six, seven, eight, test_node
 ];
